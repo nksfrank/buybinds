9d8da517cc20e53ebb6f139f88c466e6bf2f7a58
nksfrank <niklas.frank@gmail.com>
2015-05-10 01:19:54 +0200

    Add weapons UI element

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" ng-app="bbApp">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="js/jquery-1.11.2.min.js"></script>
     <script src="js/angular.js"></script>
@@ -12,9 +13,6 @@
 <body>
     <div class="wrapper" ng-controller="BindsCtrl">
         <div class="content box white round">
-            <div id="message" class="alert box round">
-                <select><option ng-repeat="item in items" value="{{item.cmd}}">{{item.name}}</option></select>
-            </div>
             <div class="clearfix">
                 <div class="left">
                     <table class="numpad">
@@ -24,10 +22,18 @@
                     </table>
                 </div>
                 <div class="right">
-                    <div ng-repeat="(key, value) in items">
-                        <h4>{{key}}</h4>
-                        <button ng-repeat="weap in value">{{weap}}"</button>
-                    </div>
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="round" placeholder="John Doe" ng-model="contact.name" ng-class="{ 'error' : err.name }">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="round" placeholder="John Doe" ng-model="contact.name" ng-class="{ 'error' : err.name }">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="round" placeholder="John Doe" ng-model="contact.name" ng-class="{ 'error' : err.name }">
+                </div>
+            </div>
+            <div ng-repeat="(key, value) in items">
+                <h4>{{key}}</h4>
+                <div class="tagg teal round" ng-repeat="(key, value) in value">
+                    {{key}}
                 </div>
             </div>
         </div>
