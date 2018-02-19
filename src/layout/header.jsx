fa9939b40a96a2d756d55c397e2a0e306483b543
niklas.frank <niklas.frank@gmail.com>
2018-02-19 08:07:49 +0100

    add title to page

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -3,6 +3,6 @@ import { Section } from './section';
 
 export const Header = ({}) => (
   <Section>
-    <header className="header">Csgo buy binds</header>
+    <header className="header">Cs:Go Buy Binds</header>
   </Section>
 );
